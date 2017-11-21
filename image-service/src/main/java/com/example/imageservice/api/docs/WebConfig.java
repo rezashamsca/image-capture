890c352a285feef44ff09e5cfd32430f2e854633
rezashams <rezashamsca@yahoo.ca>
2017-11-20 22:46:26 -0500

    Added Swagger for api doc

---
+++ image-service/src/main/java/com/example/imageservice/api/docs/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.imageservice.api.docs;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
