e5e9f2bddc5f9ea1dbdbc7fea5150e0873dc12ba
rezashams <rezashamsca@yahoo.ca>
2017-11-21 22:11:55 -0500

    Making sure Multipart is working

--- image-service/src/main/java/com/example/imageservice/service/EventImpl.java
+++ image-service/src/main/java/com/example/imageservice/service/EventImpl.java
@@ -3,13 +3,26 @@ package com.example.imageservice.service;
 import com.example.imageservice.api.EventApi;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
 public class EventImpl implements EventApi {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventImpl.class);
+
     @Override
     public void imageReceived(UUID imageId, MultipartFile file) {
         // push to Kafka
+        try {
+            final byte[] bytes = file.getBytes();
+            LOGGER.debug(String.valueOf(bytes.length));
+        }
+        catch (IOException ex) {
+            LOGGER.debug(ex.getMessage());
+        }
     }
 }
